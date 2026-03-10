4ad92b8e9721338fd7a32fd4bb2e05d7d94b5f5d
eduardo <eduardomendes418@gmail.com>
2026-03-10 12:09:37 -0300

    [POS-021]- feat FOOTER_LINKS

---
+++ constants/footer.ts
@@ -0,0 +1,7 @@
+export const FOOTER_LINKS = [
+  { name: "About us", href: "#" },
+  { name: "Services", href: "#" },
+  { name: "Use Cases", href: "#" },
+  { name: "Pricing", href: "#" },
+  { name: "Blog", href: "#" },
+];
