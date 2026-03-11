cadaa5826589a3330c38cfe327218ac390d3d5a9
eduardo <eduardomendes418@gmail.com>
2026-03-11 16:17:13 -0300

    [POS-022]- feat navLinks

---
+++ constants/navigation.ts
@@ -0,0 +1,7 @@
+export const navLinks = [
+  { name: "About us", href: "#about" },
+  { name: "Services", href: "#services" },
+  { name: "Use Cases", href: "#cases" },
+  { name: "Pricing", href: "#pricing" },
+  { name: "Blog", href: "#blog" },
+];
