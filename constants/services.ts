f96183161251004ec815a357cef592d950b9a9bf
eduardo <eduardomendes418@gmail.com>
2026-03-17 07:46:47 -0300

    [POS-025]- feat services

---
+++ constants/services.ts
@@ -0,0 +1,44 @@
+export const services = [
+  {
+    title: "Search engine optimization",
+    bg: "bg-brand-gray",
+    titleBg: "bg-brand-lime",
+    image: "/logos/seo.svg",
+    darkIcon: true,
+  },
+  {
+    title: "Pay-per-click advertising",
+    bg: "bg-brand-lime",
+    titleBg: "bg-white",
+    image: "/logos/ppc.svg",
+    darkIcon: true,
+  },
+  {
+    title: "Social Media Marketing",
+    bg: "bg-brand-dark",
+    titleBg: "bg-white",
+    image: "/logos/mm.svg",
+    darkIcon: false,
+  },
+  {
+    title: "Email Marketing",
+    bg: "bg-brand-gray",
+    titleBg: "bg-brand-lime",
+    image: "/logos/email.svg",
+    darkIcon: true,
+  },
+  {
+    title: "Content Creation",
+    bg: "bg-brand-lime",
+    titleBg: "bg-white",
+    image: "/logos/content.svg",
+    darkIcon: true,
+  },
+  {
+    title: "Analytics and Tracking",
+    bg: "bg-brand-dark",
+    titleBg: "bg-brand-lime",
+    image: "/logos/analytics.svg",
+    darkIcon: false,
+  },
+];
