3ba7894f5a51e65cb2fcf5b3ecfe6d62ae53989c
eduardo <eduardomendes418@gmail.com>
2026-03-23 20:27:30 -0300

    [POS-029]- feat Partners

--- components/sections/Partners/Partners.tsx
+++ components/sections/Partners/Partners.tsx
@@ -1,14 +1,17 @@
 import { partners } from "@/constants/partners";
+import Image from "next/image";
 
 export default function Partners() {
   return (
     <div className="container mx-auto px-4 py-12">
       <div className="flex flex-wrap justify-between items-center gap-8">
         {partners.map((partner) => (
-          <img 
+          <Image 
             key={partner} 
             src={`/logos/${partner}.svg`} 
             alt={partner} 
+            width={120}
+            height={40}
             className="h-10 w-auto"
           />
         ))}
